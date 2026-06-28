745c9618b38bbcebc9928027082eb32a41cd34a4
UmutSanci01 <umutsanci01@gmail.com>
2026-06-28 16:35:14 +0300

    Setup and Configuration

--- BTK-2D-GameEngine/main.cpp
+++ BTK-2D-GameEngine/main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-class Deneme
-{
-	public:
-		Deneme(int a)
-		{
-			std::cout << a << std::endl;
-		}
-};
 
 int main(int argc, char** argv)
 {
-	Deneme denek(25), denememek(12);
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "SDL Yuklenemedi. Hata! ->" << SDL_GetError << std::endl;
+	}
 
+	SDL_Window* window = SDL_CreateWindow(
+		"BTK_Engine",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800, 600,
+		SDL_WINDOW_SHOWN
+	);
+
+	// olay donguleri (event loop)
+	bool is_working = true;
+	SDL_Event event;
+
+	while (is_working)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				is_working = false;
+			}
+		}
+
+		SDL_Delay(16);
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return 0;
 }
