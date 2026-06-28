b7925283deffeca6472cc326aef1395f2b502c65
UmutSanci01 <umutsanci01@gmail.com>
2026-06-28 15:54:14 +0300

    Proje dosyası ekle.

---
+++ BTK-2D-GameEngine/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Deneme
+{
+	public:
+		Deneme(int a)
+		{
+			std::cout << a << std::endl;
+		}
+};
+
+int main(int argc, char** argv)
+{
+	Deneme denek(25), denememek(12);
+
+
+	return 0;
+}
